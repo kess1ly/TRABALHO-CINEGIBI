07c29e41deddf8169859997527a95af9875daddc
kess1ly <kekezinha2905@gmail.com>
2026-08-20 23:45:03 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "cinegibi";
+
+$conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
+if ($conexao->connect_error) {
+    die("Erro na conexão com o banco de dados: " . $conexao->connect_error);
+}
+
+$conexao->set_charset("utf8mb4");
